cd94e80b7464497b6d5ff3b5f71d3310ae150258
luka.danger <lukathevegan@gmail.com>
2023-05-02 11:59:14 -0600

    Add Comments
    Practicing commenting in Java with both single line // and multi-line /* comments

--- hello-world/src/Main.java
+++ hello-world/src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello, World!");
+        //This is my first lines of Java code//
+        System.out.println("Hello, World!");
     }
 }
+/* This comment is going to span multiple lines. I am using it to
+practice my skills with Java syntax. I'm not sure what else I should
+write so I am just going to keep using words to fill at least three lines.
+ */
