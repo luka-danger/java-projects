f9f7286dd2f593d663410222d4bc0bf4b3774b44
luka.danger <lukathevegan@gmail.com>
2023-05-05 16:26:13 -0600

    Add variables

--- hello-world/src/Main.java
+++ hello-world/src/Main.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         //This is my first lines of Java code//
-        System.out.println("Hello, World!");
+        Scanner scanner_one = new Scanner(System.in);
+        System.out.println("Hello, friend. What is your name?");
+        String name = scanner_one.nextLine();
+        System.out.println("Hello," + name + "! How are you doing today?");
+        String question = scanner_one.nextLine();
+        /* if(question == "good") {
+            System.out.println("That is great to hear!");
+        }
+        else if (response == "bad") {
+            System.out.println("I'm sorry to hear that");
+        }*/
+        System.out.println("Glad to hear that you are doing " + question.toLowerCase() + "!");
     }
 }
 /* This comment is going to span multiple lines. I am using it to
