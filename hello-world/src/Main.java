eef297d0224f9a48c9db5b942442188ac87deab6
luka.danger <lukathevegan@gmail.com>
2023-05-05 18:11:08 -0600

    First Java Project...Oh what fun

--- hello-world/src/Main.java
+++ hello-world/src/Main.java
@@ -8,16 +8,10 @@ public class Main {
         String name = scanner_one.nextLine();
         System.out.println("Hello," + name + "! How are you doing today?");
         String question = scanner_one.nextLine();
-        /* if(question == "good") {
-            System.out.println("That is great to hear!");
-        }
-        else if (response == "bad") {
-            System.out.println("I'm sorry to hear that");
-        }*/
         System.out.println("Glad to hear that you are doing " + question.toLowerCase() + "!");
     }
 }
 /* This comment is going to span multiple lines. I am using it to
 practice my skills with Java syntax. I'm not sure what else I should
-write so I am just going to keep using words to fill at least three lines.
+write, so I am just going to keep using words to fill at least three lines.
  */
