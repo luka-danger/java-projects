b31c6dbb669ba00fe496b93e6b3699aa3c5b1ade
luka.danger <lukathevegan@gmail.com>
2023-05-06 13:44:50 -0600

    Initial Commit
    Add print statements.

---
+++ magic-trick/src/MagicTrick.java
@@ -0,0 +1,24 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class MagicTrick {
+    public static void main(String[] args) {
+        //add strings first?
+    Scanner userInput = new Scanner(System.in);
+    System.out.println("Hello! What is your name? ");
+    String name = userInput.nextLine();
+    System.out.println("Hi, " + name + "! Now I will perform some mind reading on you.");
+    System.out.println("Think of a number between 1 and 10.");
+    System.out.println("Hit enter when have picked your number.");
+    System.out.println("Multiply the number by 2.");
+    System.out.println("Hit enter when have picked your number.");
+    System.out.println("Add ");
+    System.out.println("Hit enter when have picked your number.");
+    System.out.println("Divide the number by 2.");
+    System.out.println("Hit enter when have picked your number.");
+    System.out.println("Now, subtract the original number.");
+    System.out.println("Hit enter when have picked your number.");
+    System.out.println("I see the number in your mind...");
+    System.out.println("The number that you have now is ");
+    }
+}
