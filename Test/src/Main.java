77d2935d8c725e185d861fbf895688c9aec6fbea
luka.danger <lukathevegan@gmail.com>
2023-05-08 22:50:39 -0600

    Add additional test methods

--- Test/src/Main.java
+++ Test/src/Main.java
@@ -1,7 +1,11 @@
+//Import Java utilities
 import java.util.Scanner;
 
+//Testing different aspects of Java
+//Using this as my test program for CS1101:'Intro to Java'
 public class Main {
     public static void main(String[] arg) {
+        //Test the Upper and Lower Case Methods
         String funny = "Pickle";
         System.out.println(funny.charAt(2));
         String chilling = "I AM EASILY DISTRACTED";
@@ -12,9 +16,26 @@ public class Main {
         System.out.println(school.toUpperCase());
         System.out.println(test.toLowerCase());
         System.out.println(test2.toUpperCase());
+
+        //Test the nextLine and replace method
         Scanner hello = new Scanner(System.in);
         System.out.println("What is your name?");
         String name = hello.nextLine();
         System.out.println("Hello " + name + "! " + chilling.replace("I AM" , "YOU ARE"));
+
+        //Test more next methods
+        System.out.print("Enter 2 or more words separated by spaces: ");
+        String inputRead = hello.next();
+        System.out.println("Input read by next(): " + inputRead);
+        String stringInput = hello.next();
+        char singleChar = stringInput.charAt(0);
+        System.out.println("You entered " + singleChar + ".");
+
+        //Test string concatenation
+        int lineNumber = 1;
+        String firstString = lineNumber + ". First Place.\n";
+        lineNumber++;
+        String secondString = lineNumber + ". Second Place.";
+        System.out.println(firstString + secondString);
     }
 }
