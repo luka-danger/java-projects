bbc4ca1a2e5b02fcacfdfaedf37f91f0ed246253
luka.danger <lukathevegan@gmail.com>
2023-05-10 16:49:49 -0600

    Initial Commit
    This is a drink order app. The menu, greetings, questions, and responses are all in Polish.

---
+++ drink-order/src/drinkOrder.java
@@ -0,0 +1,28 @@
+/* Drink Menu Program (Menu in Polish)
+
+Napoje:
+Woda
+    Gazowana
+    Niegazowana
+
+Kawa
+    Czarna kawa (bez mleka)
+    Z mlekiem owsianym
+
+Herbata
+    Zielona
+        Z cytryną
+        Bez cytryny
+    Malinowa
+
+Piwo
+    Zywiec
+    Okocim
+    Tyskie
+ */
+
+public class drinkOrder {
+    public static void main(String[] args) {
+    System.out.println("Dzien dobry. Co chcesz do picia?");
+    }
+}
