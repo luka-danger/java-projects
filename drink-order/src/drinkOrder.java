785ef6d6270bd279cfcd1d2dd39b1e18129227fe
luka.danger <lukathevegan@gmail.com>
2023-05-20 17:54:40 -0600

    Add print statements

--- drink-order/src/drinkOrder.java
+++ drink-order/src/drinkOrder.java
@@ -21,8 +21,15 @@ Piwo
     Tyskie
  */
 
+import java.util.Scanner;
+
 public class drinkOrder {
     public static void main(String[] args) {
-    System.out.println("Dzien dobry. Co chcesz do picia?");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Dzien dobry. Co chcesz do picia?");
+        System.out.println("1) Woda 2) Kawa 3) Herbata 4) Piwo");
+        System.out.println("Wybierz numer: ");
+        int drinkTypeResponse = input.nextInt();
+        }
     }
 }
