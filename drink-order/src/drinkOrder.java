b45501e8507af4a38f84dc136ee069bf27572b6c
luka.danger <lukathevegan@gmail.com>
2023-05-20 18:05:25 -0600

    Add If and Else Statement

--- drink-order/src/drinkOrder.java
+++ drink-order/src/drinkOrder.java
@@ -29,7 +29,15 @@ public class drinkOrder {
         System.out.println("Dzien dobry. Co chcesz do picia?");
         System.out.println("1) Woda 2) Kawa 3) Herbata 4) Piwo");
         System.out.println("Wybierz numer: ");
-        int drinkTypeResponse = input.nextInt();
+        int drinkResponse = input.nextInt();
+        String drinkDetails = "Nie zamówiłeś napoju";
+        //Provide options depending on the drink selection
+        if(drinkResponse == 1) {
+            drinkDetails = "Woda";
+            System.out.println(drinkDetails);
+            }
+        else {
+            System.out.println(drinkDetails);
         }
     }
 }
