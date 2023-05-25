0424afb6389a3c8a6ef344efeb103728b27128e6
luka.danger <lukathevegan@gmail.com>
2023-05-25 15:58:45 -0600

    Add binary search function
    The if statement logic works for numbers found in the array; however, it is also returning numbers that aren't in the array. Will fix in the next update

--- arrays-and-loops/src/Main.java
+++ arrays-and-loops/src/Main.java
@@ -4,11 +4,25 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-    int [] scores = {187, 42, 17, 100, 19};
-    Arrays.sort(scores);
-    int lowScore = scores[0];
-    int highScore = scores[scores.length - 1];
-    System.out.println("High Score: " + highScore);
-    System.out.println("Low Score: " + lowScore);
-    }
+        Scanner input = new Scanner(System.in);
+        int[] scores = {187, 42, 17, 100, 19};
+        //Organize arrays from lowest to highest
+        Arrays.sort(scores);
+        //Sort will arrange the lowest score first
+        //Index 0 will print the first score, which will be the lowest
+        int lowScore = scores[0];
+        int highScore = scores[scores.length - 1];
+        System.out.println("High Score: " + highScore);
+        System.out.println("Low Score: " + lowScore);
+        //Search for a number in the array
+        System.out.println("Enter a value: ");
+        int searchResults = input.nextInt();
+        if (searchResults > -1) {
+            int searchLocation = Arrays.binarySearch(scores, searchResults);
+            System.out.println("Number " + searchResults + " was found at index " + searchLocation + ".");
+        }
+        else {
+            System.out.println("Number  " + searchResults + " was not found.");
+        }
+        }
 }
