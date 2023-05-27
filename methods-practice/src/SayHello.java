d792ffdfe32744cadf8583b8b1f15ef581e9fee6
luka.danger <lukathevegan@gmail.com>
2023-05-27 12:19:58 -0600

    Add comments

--- methods-practice/src/SayHello.java
+++ methods-practice/src/SayHello.java
@@ -16,17 +16,21 @@ public class SayHello {
         Scanner input = new Scanner(System.in);
         System.out.println("What is your name?: ");
         String userName = input.nextLine();
+        // Use sayHello method to return "Hello" and entered userName
         String greeting = sayHello(userName);
+        // Use saySomethingNice method to return statement and userName
         String beNice = saySomethingNice(userName);
         System.out.println(greeting + " Welcome to Java!");
         System.out.println(beNice);
         String response = input.nextLine();
-
+        // If user types "thank you", the output will return "you're welcome"
+        // If any other response or action given, the code continues without an output
         if(response.equalsIgnoreCase("thank you")) {
             System.out.println("You're welcome, " + userName + ".");
         }
         int[] myArray = {8, 10, 4, 12, 2, 6};
         System.out.println("Original: " + Arrays.toString(myArray));
+        // Use myArray method to sort array from smallest to largest integer
         sortArray(myArray);
         System.out.println("New: " + Arrays.toString(myArray));
     }
