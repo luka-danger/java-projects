9e9aeed52728c430df8f254efa6894c7b860e252
luka.danger <lukathevegan@gmail.com>
2023-05-27 12:15:51 -0600

    Add new class
    Use .sort() to arrange array in order from smallest to largest integers.

--- methods-practice/src/SayHello.java
+++ methods-practice/src/SayHello.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class SayHello {
     static String sayHello(String name) {
@@ -6,6 +7,9 @@ public class SayHello {
     }
     static String saySomethingNice(String name) {
         return "You look nice today, " + name + ".";
+    }
+    static void sortArray(int[] values) {
+        Arrays.sort(values);
     }
 
     public static void main(String[] args) {
@@ -21,5 +25,9 @@ public class SayHello {
         if(response.equalsIgnoreCase("thank you")) {
             System.out.println("You're welcome, " + userName + ".");
         }
+        int[] myArray = {8, 10, 4, 12, 2, 6};
+        System.out.println("Original: " + Arrays.toString(myArray));
+        sortArray(myArray);
+        System.out.println("New: " + Arrays.toString(myArray));
     }
 }
