9c7ef55f7bdde84712dcca7dd610b76018564e89
luka.danger <lukathevegan@gmail.com>
2023-05-27 23:28:31 -0600

    Initial Commit
    Practice with building a RESTful service

---
+++ spring/demo/src/main/java/com/example/demo/Greeting.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+public class Greeting {
+
+    private Long id;
+    private String content;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Greeting(Long id, String content) {
+            this.id = id;
+            this.content = content;
+
+    }
+}
