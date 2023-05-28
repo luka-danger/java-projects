3c6da8b07e981147d449c6a05953ec612d9067b2
luka.danger <lukathevegan@gmail.com>
2023-05-27 23:32:05 -0600

    Change Get Mapping to Request Mapping

--- spring/demo/src/main/java/com/example/demo/GreetingController.java
+++ spring/demo/src/main/java/com/example/demo/GreetingController.java
@@ -1,8 +1,6 @@
 package com.example.demo;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -11,7 +9,7 @@ public class GreetingController {
 
     private static final String template = "Hello, %s!";
     private final AtomicLong counter = new AtomicLong();
-    @GetMapping("/greeting")
+    @RequestMapping(method = RequestMethod.GET, path = "/greeting")
     Greeting greeting(@RequestParam(defaultValue = "World") String name) {
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
