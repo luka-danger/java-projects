e75e5d5383706e7305de09b6d156c91be8565a6b
luka.danger <lukathevegan@gmail.com>
2023-05-28 23:29:32 -0600

    Create ArrayList
    This is where things are going to get tricky. I need to figure out a way to keep the items in the list stored for longer than the duration of one run of the program so I can use the other methods.

--- RunTracker/src/Main.java
+++ RunTracker/src/Main.java
@@ -1,65 +1,68 @@
 import java.lang.reflect.Array;
 import java.util.Arrays;
-import java.util.HashSet;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
+        public static void addRun() {
+            ArrayList runs = new ArrayList();
+            Scanner input = new Scanner(System.in);
+            System.out.println("Enter mileage: ");
+            double mileage = input.nextDouble();
+            System.out.println("Enter elevation gain: ");
+            int elevationGain = input.nextInt();
+            System.out.println("Enter run duration (in minutes): ");
+            double runTime = input.nextDouble();
+            // Ensure the blank space isn't being used in the runNotes variable
+            input.nextLine();
+            System.out.println("Enter run notes: ");
+            String runNotes = input.nextLine();
 
-    HashSet runs = new HashSet();
-    public static void addRun() {
-        Scanner input = new Scanner(System.in);
-        System.out.println("Enter mileage: ");
-        double mileage = input.nextDouble();
-        System.out.println("Enter elevation gain: ");
-        int elevationGain = input.nextInt();
-        System.out.println("Enter run duration (in minutes): ");
-        double runTime = input.nextDouble();
-        // Ensure the blank space isn't being used in the runNotes variable
-        input.nextLine();
-        System.out.println("Enter run notes: ");
-        String runNotes = input.nextLine();
+            runs.add(mileage + " miles");
+            runs.add(elevationGain + " feet");
+            runs.add(runTime + " minutes");
+            runs.add("Notes: " + runNotes);
+            System.out.println(runs);
+            System.out.println("Run added!");
+        }
 
+        public static void main(String[] args) {
+            Scanner select = new Scanner(System.in);
+            System.out.print("Welcome to the Run Tracker!");
+            System.out.println(" Please select an option from the Start Menu.");
+            StartMenu startMenuObject = new StartMenu();
+            startMenuObject.displayMenu();
+            // Improve UI
+            System.out.print("> ");
+            int choose = select.nextInt();
+            // Select 1 for 1. Add Run
+            if (choose == 1) {
+                // Call addRun method
+                addRun();
+            }
+            // View Runs
+            else if (choose == 2) {
 
-        System.out.println(runs);
-        System.out.println("Run added!");
+            }
+            // Search Runs
+            else if (choose == 3) {
+
+            }
+            // Delete Run
+            else if (choose == 4) {
+
+            }
+            // Quit
+            else if (choose == 5) {
+
+            }
+            // Invalid option, please try again
+            else {
+
+            }
+        }
     }
 
-    public static void main(String[] args) {
-        Scanner select = new Scanner(System.in);
-        System.out.print("Welcome to the Run Tracker!");
-        System.out.println(" Please select an option from the Start Menu.");
-        StartMenu startMenuObject = new StartMenu();
-        startMenuObject.displayMenu();
-        // Improve UI
-        System.out.print("> ");
-        int choose = select.nextInt();
-        // Select 1 for 1. Add Run
-        if(choose == 1) {
-            // Call addRun method
-            addRun();
-        }
-        // View Runs
-        else if(choose == 2) {
-
-        }
-        // Search Runs
-        else if(choose == 3) {
-
-        }
-        // Delete Run
-        else if(choose == 4) {
-
-        }
-        // Quit
-        else if(choose == 5) {
-
-        }
-        // Invalid option, please try again
-        else {
-
-        }
-    }
-}
 
 
