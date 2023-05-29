cb9f837ce34a9ffc93db29fbd5327bd289e60953
luka.danger <lukathevegan@gmail.com>
2023-05-28 20:15:56 -0600

    Create start menu object and displayMenu method
    I started an if statement to test if the start menu works. It does

--- RunTracker/src/Main.java
+++ RunTracker/src/Main.java
@@ -5,13 +5,17 @@ import java.util.Scanner;
 
 public class Main {
 
-
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Welcome to the Run Tracker!");
         System.out.println(" Please select an option from the Start Menu.");
-
-        
+        StartMenu startMenuObject = new StartMenu();
+        startMenuObject.displayMenu();
+        int choose = input.nextInt();
+        //Test if startMenuObject works(Delete later)
+        if(choose == 1) {
+            System.out.println("Hi");
+        }
     }
 }
 
