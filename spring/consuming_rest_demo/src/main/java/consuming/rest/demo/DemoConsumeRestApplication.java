4c1682fb4dfb46502129c6365aaacf71fef9df9a
luka.danger <lukathevegan@gmail.com>
2023-05-29 22:23:27 -0600

    Initial commit for Consuming a RESTful Web Service Spring Tutorial

---
+++ spring/consuming_rest_demo/src/main/java/consuming/rest/demo/DemoConsumeRestApplication.java
@@ -0,0 +1,13 @@
+package consuming.rest.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoConsumeRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoConsumeRestApplication.class, args);
+	}
+
+}
---
+++ spring/consuming_rest_demo/src/test/java/consuming/rest/demo/DemoConsumeRestApplicationTests.java
@@ -0,0 +1,13 @@
+package consuming.rest.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoConsumeRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
