d609788f205acec02bddc0d83d8489a542c2d7cb
luka.danger <lukathevegan@gmail.com>
2023-05-29 22:03:46 -0600

    Add and import AtomicLong

--- spring/demo/src/main/java/com/example/demo/GreetingController.java
+++ spring/demo/src/main/java/com/example/demo/GreetingController.java
@@ -4,8 +4,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @RestController
 public class GreetingController {
+
+    private static final String template = "Hello, %s";
+    private final AtomicLong counter = new AtomicLong();
 
     @GetMapping("/greeting")
     Greeting greeting(@RequestParam String name) {
