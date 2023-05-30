5d7af3af3912d9ef5a42c2adff6b9eddd9b8f43f
luka.danger <lukathevegan@gmail.com>
2023-05-30 09:52:45 -0600

    Add if / else statements

--- codeChallenges/FizzBuzz/src/FizzBuzz.java
+++ codeChallenges/FizzBuzz/src/FizzBuzz.java
@@ -6,7 +6,15 @@ public class FizzBuzz {
         System.out.println("Enter an integer: ");
         int num = input.nextInt();
         for(int i = 1; i <= num; i++) {
-
+            if(i % 3 == 0) {
+                System.out.println("Fizz");
+            }
+            else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            }
+            else {
+                System.out.println(i);
+            }
         }
     }
 }
