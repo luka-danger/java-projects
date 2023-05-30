ef58847438a94138c6d367f6d26ac24247791788
luka.danger <lukathevegan@gmail.com>
2023-05-30 09:51:56 -0600

    Create for loop

--- codeChallenges/FizzBuzz/src/FizzBuzz.java
+++ codeChallenges/FizzBuzz/src/FizzBuzz.java
@@ -4,5 +4,9 @@ public class FizzBuzz {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter an integer: ");
+        int num = input.nextInt();
+        for(int i = 1; i <= num; i++) {
+
+        }
     }
 }
