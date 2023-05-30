20491445c83d32585a09331ffe10e35ea96f6530
luka.danger <lukathevegan@gmail.com>
2023-05-29 22:01:59 -0600

    Add GetMapping and return Greeting method

--- spring/demo/src/main/java/com/example/demo/GreetingController.java
+++ spring/demo/src/main/java/com/example/demo/GreetingController.java
@@ -1,7 +1,14 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GreetingController {
+
+    @GetMapping("/greeting")
+    Greeting greeting(@RequestParam String name) {
+        return new Greeting()
+    }
 }
