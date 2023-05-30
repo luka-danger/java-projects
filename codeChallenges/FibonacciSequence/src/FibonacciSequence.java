980031d26348939929ad2f2c133b02c0b2ea56ac
luka.danger <lukathevegan@gmail.com>
2023-05-30 11:10:19 -0600

    Create additional variables

--- codeChallenges/FibonacciSequence/src/FibonacciSequence.java
+++ codeChallenges/FibonacciSequence/src/FibonacciSequence.java
@@ -1,6 +1,13 @@
 import java.util.Scanner;
 
 public class FibonacciSequence {
+    static int n1=0, n2=1, n3=0;
+    static void Fibonacci(int count){
+        if(count > 0){
+            n3 = n1 + n2;
+            n1 = n2;
+            n2 = n3;
+        }
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter an integer ");
