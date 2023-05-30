479af0c222ea6422da49753f664ce6488c61231d
luka.danger <lukathevegan@gmail.com>
2023-05-30 11:07:40 -0600

    Create n variable

--- codeChallenges/FibonacciSequence/src/FibonacciSequence.java
+++ codeChallenges/FibonacciSequence/src/FibonacciSequence.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class FibonacciSequence {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter an integer ");
+        int n = input.nextInt();
 
+    }
     }
 }
