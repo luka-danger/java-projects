bf34680422aa4c25b4fc93b5b086284337bbc258
luka.danger <lukathevegan@gmail.com>
2023-05-30 11:13:35 -0600

    Change code
    The last code did not work the way I planned. Trying something different

--- codeChallenges/FibonacciSequence/src/FibonacciSequence.java
+++ codeChallenges/FibonacciSequence/src/FibonacciSequence.java
@@ -1,18 +1,14 @@
 import java.util.Scanner;
 
 public class FibonacciSequence {
-    static int n1=0, n2=1, n3=0;
-    static void Fibonacci(int count){
-        if(count > 0){
-            n3 = n1 + n2;
-            n1 = n2;
-            n2 = n3;
-        }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Enter an integer ");
-        int n = input.nextInt();
+        System.out.println("Enter an integer: ");
+        int count = input.nextInt();
+        if (count > 0) {
+            System.out.println();
 
-    }
+        }
     }
 }
