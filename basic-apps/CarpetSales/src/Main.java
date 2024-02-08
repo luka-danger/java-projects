380f9d7c15092835f590fd2a0e5b233c4302da57
luka-danger <lukathevegan@gmail.com>
2024-02-08 13:43:33 -0700

    Add initial variables

--- basic-apps/CarpetSales/src/Main.java
+++ basic-apps/CarpetSales/src/Main.java
@@ -4,5 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner scnr = new Scanner(System.in);
 
+        System.out.println("How many orders would you like to process?: ");
+        int numOrders = scnr.nextInt();
+        System.out.println(numOrders);
+
+        double totalSales = 0;
     }
 }
