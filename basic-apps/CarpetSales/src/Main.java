533af02383a8d54c27ab47d14519ef0522716008
luka-danger <lukathevegan@gmail.com>
2024-02-08 13:45:29 -0700

    Create for loop and add initial inputs

--- basic-apps/CarpetSales/src/Main.java
+++ basic-apps/CarpetSales/src/Main.java
@@ -9,5 +9,15 @@ public class Main {
         System.out.println(numOrders);
 
         double totalSales = 0;
+
+        for (int orderNum = 1; orderNum <= numOrders; orderNum++) {
+            System.out.println("Enter price per square foot: ");
+            double pricePer = scnr.nextDouble();
+            System.out.println("Enter room width: ");
+            int roomWidth = scnr.nextInt();
+            System.out.println("Enter room length: ");
+            int roomLength = scnr.nextInt();
+            int roomArea = roomWidth * roomLength;
+        }
     }
 }
