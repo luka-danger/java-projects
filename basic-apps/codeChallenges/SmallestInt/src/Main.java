6d3089dc5781fb077e46a8d28a91d275271d34ed
luka-danger <lukathevegan@gmail.com>
2024-02-09 12:07:08 -0700

    Use ternary operator to find smallest integer
    tempSmall compares first and second, smallestInt compares tempSmall to third

--- basic-apps/codeChallenges/SmallestInt/src/Main.java
+++ basic-apps/codeChallenges/SmallestInt/src/Main.java
@@ -3,8 +3,17 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner read = new Scanner(System.in);
-        int first;
-        int second;
-        int third; 
+        System.out.println("Enter a number: ");
+        int first = read.nextInt();
+        System.out.println("Enter another number: ");
+        int second = read.nextInt();
+        System.out.println("Enter a third number: ");
+        int third = read.nextInt();
+
+        int tempSmall = (first < second) ? first : second;
+
+        int smallestInt = (tempSmall < third) ? tempSmall : third;
+
+        System.out.println("Smallest number: " + smallestInt);
     }
 }
