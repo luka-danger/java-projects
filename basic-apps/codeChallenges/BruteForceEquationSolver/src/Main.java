aa33c1555b6c11e3c463aa6da2eba43701f22085
luka-danger <lukathevegan@gmail.com>
2024-02-13 14:42:34 -0700

    Create variables and nested for loops
    Ughhh, O(n^2);

--- basic-apps/codeChallenges/BruteForceEquationSolver/src/Main.java
+++ basic-apps/codeChallenges/BruteForceEquationSolver/src/Main.java
@@ -3,5 +3,31 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+
+        int ax = scan.nextInt();
+        int by = scan.nextInt();
+        int answer = scan.nextInt();
+        int algebra;
+
+        int cx = scan.nextInt();
+        int dy = scan.nextInt();
+        int answer2 = scan.nextInt();
+        int algebra2;
+        boolean hasSolution = false;
+
+        for (int i = -10; i <= 10; i++) {
+            for (int j = -10; j <= 10; j++) {
+                algebra = (ax * i) + (by * j);
+                algebra2 = (cx * i) + (dy * j);
+                if (algebra == answer && algebra2 == answer2) {
+                    hasSolution = true;
+                    break;
+                }
+
+            }
+        }
+
+
+
     }
 }
