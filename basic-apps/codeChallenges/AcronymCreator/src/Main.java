ce6b41d3a558317bfd85a2a371f3b0edce5e32f5
luka-danger <lukathevegan@gmail.com>
2024-02-22 23:22:02 -0700

    Finish program
    IntelliJ suggested using a StringBuilder and an enhanced for loop. Thanks, IntelliJ

--- basic-apps/codeChallenges/AcronymCreator/src/Main.java
+++ basic-apps/codeChallenges/AcronymCreator/src/Main.java
@@ -3,18 +3,25 @@ import java.util.Scanner;
 public class Main {
     public static String createAcronym(String userPhrase) {
         String[] words = userPhrase.split(" ");
-        String acronym = "";
+        StringBuilder acronym = new StringBuilder();
 
-        for (int i = 0; i < words.length; i++) {
-            
+        for (String word : words) {
+            if (Character.isUpperCase(word.charAt(0))) {
+                acronym.append(Character.toUpperCase(word.charAt(0))).append(".");
+            }
         }
 
-        return acronym;
+        return acronym.toString();
     }
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        int count = 0;
 
-
+        while (count < 8) {
+            System.out.println("Enter a phrase to abbreviate: ");
+            System.out.println(createAcronym(scan.nextLine()));
+            count ++;
+        }
     }
 }
