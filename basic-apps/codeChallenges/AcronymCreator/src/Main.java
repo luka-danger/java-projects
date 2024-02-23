e996cc3c75101571f098643ec9feaf4959088f48
luka-danger <lukathevegan@gmail.com>
2024-02-22 23:05:23 -0700

    Create method...incomplete

--- basic-apps/codeChallenges/AcronymCreator/src/Main.java
+++ basic-apps/codeChallenges/AcronymCreator/src/Main.java
@@ -1,6 +1,17 @@
 import java.util.Scanner;
 
 public class Main {
+    public static String createAcronym(String userPhrase) {
+        String[] words = userPhrase.split(" ");
+        String acronym = "";
+
+        for (int i = 0; i < words.length; i++) {
+            
+        }
+
+        return acronym;
+    }
+
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
