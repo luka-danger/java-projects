80a03f71e700518748da7f6b6ba1f1954384b05a
luka-danger <lukathevegan@gmail.com>
2024-02-25 23:03:17 -0700

    Complete Program

--- basic-apps/codeChallenges/FindSumAndSmallest/src/Main.java
+++ basic-apps/codeChallenges/FindSumAndSmallest/src/Main.java
@@ -5,13 +5,16 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         ArrayList<Integer> nums = new ArrayList<>();
+        System.out.println("Enter non-negative integers to add to the ArrayList: ");
 
         while(scan.hasNextInt()) {
             int num = scan.nextInt();
+            nums.add(num);
             if (num < 0) {
+                System.out.print("You entered a negative integer. ");
+                System.out.println("No more integers will be added to the Array List");
                 break;
             }
-            nums.add(num);
         }
 
         int smallest = nums.getFirst();
@@ -20,7 +23,15 @@ public class Main {
                 smallest = num;
             }
         }
+        System.out.println("Smallest: " + smallest);
 
-        
+        int sum = 0;
+        for(int num : nums) {
+            if (num > 0) {
+                sum = sum + num;
+            }
+        }
+        System.out.println("Sum: " + sum);
+
     }
 }
