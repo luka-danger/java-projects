4ee0a83139631e9805a36e69554ff1c1f1e079d2
luka-danger <lukathevegan@gmail.com>
2024-02-25 22:50:43 -0700

    Create while loop

--- basic-apps/codeChallenges/FindSumAndSmallest/src/Main.java
+++ basic-apps/codeChallenges/FindSumAndSmallest/src/Main.java
@@ -5,5 +5,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         ArrayList<Integer> nums = new ArrayList<>();
+
+        while(scan.hasNextInt()) {
+            int num = scan.nextInt();
+            if (num < 0) {
+                break;
+            }
+            nums.add(num);
+        }
     }
 }
